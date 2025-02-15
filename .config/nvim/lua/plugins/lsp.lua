421f6f5c3e0fcc76b0ad065fc342638866e58e5b
Jakob Weimar <jakob.weimar@gmx.de>
2025-02-15 23:38:14 +0100

    updated configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,19 @@
 vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 
+
+-- Don't forget to install shellcheck!
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
+  callback = function()
+    vim.lsp.start({
+      name = 'bash-language-server',
+      cmd = { 'bash-language-server', 'start' },
+    })
+  end,
+})
+
+
 return {
 	'williamboman/mason.nvim',
 	{
@@ -8,7 +21,7 @@ return {
 		config = function()
 			require("mason").setup()
 			require("mason-lspconfig").setup {
-				ensure_installed = { "jdtls", "vimls", "hydra_lsp", "eslint", "bashls", "lua_ls", "pyright" },
+				ensure_installed = { "jdtls", "vimls", "hydra_lsp", "eslint", "bashls", "lua_ls", "pyright", "bashls" },
 				-- "nginx_language-server"
 			}
 		end,
