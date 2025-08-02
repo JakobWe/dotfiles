f0d986a4b86a93dde4637d07b7a83bc7c5818250
Jakob Weimar <jakob.weimar@gmx.de>
2025-08-02 22:17:15 +0200

    update some nvim lsp bindings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,16 +4,19 @@ vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover)
 
+vim.keymap.set("n", "<F2>", function() vim.diagnostic.jump({count=1,float=true}) end)
+vim.keymap.set("n", "<F14>", function() vim.diagnostic.jump({count=-1,float=true}) end)
+
 vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
 })
 
 return {
